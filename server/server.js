85942b5d96e243a9742777ba4b34de3233a3a1d0
lxy <linxinyu@shiyue.com>
2026-06-08 18:32:36 +0800

    新建websocket服务器

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import { WebSocketServer } from "ws";
+
+//创建websocket服务器，并监听8080端口
+const wss = new WebSocketServer({port:8080})
+console.log('服务器已经启动，地址为：ws://localhost:8080')
+
+
+wss.on('connection',(ws)=>{
+    console.log('有用户连接上了')
+    ws.on('message',(data)=>{
+        const message = data.toString()
+        console.log('接收到的信息为：',message)
+
+        wss.clients.forEach(client=>{
+            if(client.readyState===1){
+                client.send(message)
+            }
+        })
+    })
+    ws.on('close',()=>{
+        console.log('有用户离开了')
+    })
+})
